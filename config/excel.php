53b8e5c364fbc0867241a43badf19a9738d4c9c0
putraaxzy <wibowo.yunanto41@smk.belajar.id>
2026-01-02 10:02:25 +0700

    update Excel configuration for improved export handling

--- config/excel.php
+++ config/excel.php
@@ -128,7 +128,7 @@ return [
             'enclosure'        => '"',
             'escape_character' => '\\',
             'contiguous'       => false,
-            'input_encoding'   => Csv::GUESS_ENCODING,
+            'input_encoding'   => 'UTF-8',
         ],
 
         /*
